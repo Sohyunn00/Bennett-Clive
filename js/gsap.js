eeb6acbac450f6c8a2f03fd527a453804edb682d
sohyunkim <alice3405@naver.com>
2025-03-04 18:22:23 +0900

    [add] 인트로

--- js/gsap.js
+++ js/gsap.js
@@ -1,3 +1,5 @@
+gsap.registerPlugin(ScrollTrigger);
+
 // 메인 동영상 슬라이드 --------------------------------------
 
 const tl = gsap.timeline({
@@ -74,52 +76,80 @@ const tl2 = gsap.timeline({
   scrollTrigger: {
     trigger: ".sec2",
     start: "50% 50%",
-    end: "100%, 50%",
+    end: "100%, 100%",
     scrub: 0,
+    markers: true,
   },
 });
 
-for (let i = 1; i < $(".left_title p").length; i++) {
+for (let i = 1; i < $(".left_list li").length; i++) {
   tl2
-    .to(
-      `.left_title p:nth-child(${i})`,
-      {
-        scale: 1,
-      },
-      `a${i - 1}`
-    )
-    .to(
-      `.left_title p:nth-child(${i + 1})`,
-      {
-        yPercent: `-${i}00`,
-      },
-      `a${i - 1}`
-    )
-    .to(
-      `.left_title p:nth-child(n+${i + 1})`,
-      {
-        yPercent: `-${i}00`,
-      },
-      `a${i - 1}`
-    );
+    .to(`.left_list li:nth-child(${i})`, {
+      scale: 1,
+    })
+    .to(`.left_list li:nth-child(${i + 1})`, {
+      yPercent: `-${i}00`,
+    })
+    .to(`.left_list li:nth-child(n+${i + 1})`, {
+      yPercent: `-${i}00`,
+    });
 }
 
 // 오른쪽 텍스트 슬라이드 --------------------------------------
 
-for (let i = 1; i <= $(".right_title p").length; i++) {
+for (let i = 1; i <= $(".right_list li").length; i++) {
   tl2
-    .to(
-      `.right_title p:nth-child(${i})`,
-      {
-        scale: 0,
-      },
-      `a${i - 1}`
-    )
-    .to(
-      `.right_title p:nth-child(n+${i + 1})`,
-      {
-        yPercent: `-${i}00`,
-      },
-      `a${i - 1}`
-    );
+    .to(`.right_list li:nth-child(${i})`, {
+      scale: 0,
+    })
+    .to(`.right_list li:nth-child(n+${i + 1})`, {
+      yPercent: `-${i}00`,
+    });
 }
+
+// sec3 브랜드 & 효과 --------------------------------------
+
+gsap.utils.toArray(".brand_row").forEach((row, i) => {
+  let left = row.querySelector(".left"),
+    right = row.querySelector(".right");
+
+  gsap.to(left, {
+    xPercent: -10, // 왼쪽 이동
+    opacity: 0.8,
+    scrollTrigger: {
+      trigger: row,
+      end: "top 20%",
+      scrub: 1,
+      ease: "power2.out",
+      stagger: 0.1,
+      toggleActions: "play reverse play reverse",
+    },
+  });
+
+  gsap.to(right, {
+    xPercent: 10, // 오른쪽 이동
+    opacity: 0.8,
+    scrollTrigger: {
+      trigger: row,
+      start: "top 80%",
+      end: "top 20%",
+      scrub: 1,
+      ease: "power2.out",
+      stagger: 0.1,
+      toggleActions: "play reverse play reverse",
+    },
+  });
+});
+// 푸터 제목 --------------------------------------
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: "footer",
+      start: "0% 95%",
+      end: "100%, 100%",
+      scrub: 1,
+      markers: true,
+    },
+  })
+  .to(".footer_title a:first-child", { scale: 1 })
+  .to(".footer_title a:last-child", { scale: 1 });
--- js/js.js
+++ js/js.js
@@ -1,5 +1,21 @@
 "use strict";
 
+// 인트로 화면 ------------------------------
+
+window.addEventListener("load", function () {
+  const introElement = document.querySelector(".intro");
+
+  function hideIntro() {
+    introElement.style.opacity = 0;
+    setTimeout(() => {
+      introElement.style.display = "none"; // 완전히 숨김
+    }, 500); // 0.5초 뒤 display: none 적용
+  }
+
+  introElement.style.opacity = 1;
+  setTimeout(hideIntro, 3000); // 3초 후 사라짐
+});
+
 // 부드러운 스크롤 ------------------------------
 const lenis = new Lenis();
 
@@ -28,3 +44,44 @@ $(function () {
     }
   });
 });
+
+// 세계 시간 -----------------------------------
+
+let currentHour = 0;
+let currentMinute = 0;
+
+const printTime = () => {
+  function getTime(offset) {
+    const current = new Date();
+    const utc = current.getTime() + current.getTimezoneOffset() * 60 * 1000;
+    const diff = offset * 60 * 60 * 1000;
+    const currentTime = new Date(utc + diff);
+
+    currentHour = String(currentTime.getHours()).padStart(2, "0");
+    currentMinute = String(currentTime.getMinutes()).padStart(2, "0");
+  }
+
+  const clockEls = document.querySelectorAll(".clock");
+
+  clockEls.forEach(function (clockEl, index) {
+    const hour = clockEl.querySelector(".hour");
+    const minute = clockEl.querySelector(".minute");
+
+    if (clockEl.dataset.zone === "new-york") {
+      getTime(-4);
+    }
+    if (clockEl.dataset.zone === "los-angeles") {
+      getTime(-7);
+    }
+    if (clockEl.dataset.zone === "miami") {
+      getTime(-4);
+    }
+
+    hour.textContent = currentHour;
+    minute.textContent = currentMinute;
+  });
+  setTimeout(() => {
+    printTime();
+  }, 60000);
+};
+printTime();
